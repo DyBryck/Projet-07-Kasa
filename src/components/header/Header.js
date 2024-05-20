b4a97787c67291ca9d565e81c3bad0e43049c8d3
DyBryck <150355330+DyBryck@users.noreply.github.com>
2024-05-20 19:40:00 +0200

    header stylisé

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo-rouge.png";
+import "./style.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about-us">À propos</Link>
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Logo Kasa" />
+      <nav className="header__nav">
+        <ul className="header__nav__list">
+          <Link className="header__nav__list__link" to="/">
+            Accueil
+          </Link>
+          <Link className="header__nav__list__link" to="/about-us">
+            À propos
+          </Link>
+        </ul>
       </nav>
     </header>
   );
